a858c4488b31f9e2cd2a405d07cb36cdea86245a
Kang-min Liu <gugod@gugod.org>
2014-10-30 16:11:22 +0100

    deps.

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
 (defproject cvt "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :license {:name "CC0 1.0 Universal"
+            :url "http://creativecommons.org/publicdomain/zero/1.0/"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [opencv/opencv "2.4.10"]
+                 [opencv/opencv-native "2.4.10"]]
+  :injections [(clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)]
+  :plugins [[lein-localrepo "0.5.2"]]
   :main ^:skip-aot cvt.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
