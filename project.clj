d84551bab1b226c7fed9b178a41b6a9f8d38f3bf
Kang-min Liu <gugod@gugod.org>
2014-11-12 21:58:03 +0100

    use clj-exif-orientation to transparently handle disoriented jpeg.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "CC0 1.0 Universal"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-exif-orientation "0.2.0"]
                  [opencv/opencv "2.4.9"]
                  [opencv/opencv-native "2.4.9"]]
   :injections [(clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)]
--- src/cvt/core.clj
+++ src/cvt/core.clj
@@ -1,8 +1,17 @@
 (ns cvt.core
+  (:import
+   (org.opencv.highgui Highgui))
   (:require
+   [clj-exif-orientation.core :as ceo]
    [clojure.string :as str]
-   [clojure.java.io :as io]
-   ))
+   [clojure.java.io :as io]))
+
+(defn imread-reoriented
+  ""
+  [image-path]
+  (let [temp-file "/tmp/cvt-reoriented.jpg"]
+    (ceo/without-exif (io/file image-path) (io/file temp-file))
+    (Highgui/imread temp-file)))
 
 (defn image-file-seq
   "file-seq + filter that gives only a sequence of image files."
