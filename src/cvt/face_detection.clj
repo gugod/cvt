9da77d3984cb74334d5b3ca2606ad80fd448a0e4
Kang-min Liu <gugod@gugod.org>
2014-11-14 23:12:59 +0100

    try to use "eye" detection as some additional information...

--- src/cvt/face_detection.clj
+++ src/cvt/face_detection.clj
@@ -18,17 +18,41 @@
   [image-path]
   (let [image   (imread-reoriented image-path)
         image2  (Mat.)
+        eye-classifier  (CascadeClassifier. (.getPath (io/file (io/resource "cascades/haarcascades/haarcascade_eye.xml"))))
+        eye-detected    (MatOfRect.)
         face-classifier (CascadeClassifier. (.getPath (io/file (io/resource "cascades/lbpcascades/lbpcascade_frontalface.xml"))))
-        face-detected   (atom [])]
+        face-detected   (MatOfRect.)]
 
     (if (> (.channels image) 1)
       (do
         (Imgproc/cvtColor image image2 Imgproc/COLOR_RGB2GRAY)
         (Imgproc/equalizeHist image2 image)))
 
-    (reset! face-detected (MatOfRect.))
-    (.detectMultiScale face-classifier image @face-detected)
-    @face-detected))
+    (.detectMultiScale face-classifier image face-detected)
+    (.detectMultiScale eye-classifier  image eye-detected)
+    (println (str "face: " (detection-to-string face-detected)))
+    (println (str "eye: " (detection-to-string eye-detected)))
+
+    (filter (fn [face-rect]
+              (let [fx1 (.x face-rect)
+                    fx2 (+ (.x face-rect) (.width face-rect))
+                    fy1 (.y face-rect)
+                    fy2 (+ (.y face-rect) (.height face-rect))]
+                (> (.size (filter (fn [eye-rect]
+                                    (let [ex1 (.x eye-rect)
+                                          ex2 (+ (.x eye-rect) (.width eye-rect))
+                                          ey1 (.y eye-rect)
+                                          ey2 (+ (.y eye-rect) (.height eye-rect))]
+                                      (> (.size (filter
+                                                 (fn [p]
+                                                   (and (< fx1 (first p)) (< (first p) fx2)
+                                                        (< fy1 (second p)) (< (second p) fy2)))
+                                                 [[ex1 ey1] [ex1 ey2] [ex2 ey1] [ex2 ey2]]))
+                                         0)))
+                                  (.toArray face-detected)))
+                   0)))
+            (.toArray face-detected))))
+
 
 (defn -main
   "I can do face detection: lein run -m cvt.face-detection ~/Pictures/"
